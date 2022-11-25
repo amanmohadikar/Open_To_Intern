6ff1e1512a25225c8cadee3e8da1b5cd8a7de2f3
Aman <amanmohadikar111@gmail.com>
2022-11-25 19:16:18 +0530

    FrontEnd Part

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,6 @@ export const scaleFactor = 621 / 900
 const baseUrl = 'http://localhost:3001/functionup'
 export const collegeDetailsApi = collegeName => `${baseUrl}/collegeDetails?collegeName=${collegeName}`
 export const placementInterestApi = `${baseUrl}/interns`
+
+
+// placementInterestApi collegeDetails
